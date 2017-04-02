136ccfcfcb88917d1c19dee48806c8966e9e2c97
adricres96 <adricres@hotmail.es>
2017-04-02 18:46:51 +0200

    Add files via upload
    ruta para obtener la valoración de un profesor

---
+++ rutas/valoraciones.js
@@ -0,0 +1,13 @@
+
+
+module.exports = function (app) 
+{
+	app.get('/api/valoraciones/:id', function (req, res) {
+		Profesor.findOne({userName: req.params.id},function(err,data)){
+			Valoraciones.find({profesor: data._id}).populate('alumno','userName').exec(function(err,data){
+				console.log(data.puntuacion);
+				res.json(data);
+			});
+		});
+	});
+};
