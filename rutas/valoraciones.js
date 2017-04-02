bad7e829b69d43f09187564782497616846801bc
adricres96 <adricres@hotmail.es>
2017-04-02 23:12:44 +0200

    Add files via upload

--- rutas/valoraciones.js
+++ rutas/valoraciones.js
@@ -1,9 +1,13 @@
 
+require('../modelos/alumno');
+require('../modelos/profesor');
+require('../modelos/valoraciones');
+
 
 module.exports = function (app) 
 {
 	app.get('/api/valoraciones/:id', function (req, res) {
-		Profesor.findOne({userName: req.params.id},function(err,data)){
+		Profesor.findOne({userName: req.params.id},function(err,data){
 			Valoraciones.find({profesor: data._id}).populate('alumno','userName').exec(function(err,data){
 				console.log(data.puntuacion);
 				res.json(data);
