f4b4f68068594cf6e124425411d850a9d3b0fe68
adricres96 <adricres@hotmail.es>
2017-04-20 18:26:58 +0200

    Update valoracion.js

--- modelos/valoracion.js
+++ modelos/valoracion.js
@@ -7,7 +7,7 @@ var Schema = mongoose.Schema;
 var valoracionSchema = new Schema({ 
 	alumno: {type: Schema.Types.ObjectId, ref: 'Alumno',required:true},
 	profesor: {type: Schema.Types.ObjectId, ref: 'Profesor',required:true},
-	puntuacion: {type: Number,required:true,}
+	puntuacion: {type: Number,required:true}
 });
  
 module.exports=mongoose.model('Valoracion',valoracionSchema);
