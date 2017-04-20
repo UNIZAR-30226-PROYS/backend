a92ef51daedfd0ccbbd7c1d7dcf77bf3241be41c
adricres96 <adricres@hotmail.es>
2017-04-21 01:26:41 +0200

    Update valoraciones.js

--- rutas/valoraciones.js
+++ rutas/valoraciones.js
@@ -7,11 +7,9 @@ require('../modelos/valoracion');
 module.exports = function (app) 
 {
 	app.get('/api/valoraciones/:id', function (req, res) {
-		Profesor.findOne({userName: req.params.id},function(err,data){
-			Valoraciones.find({profesor: data._id}).populate('alumno','userName').exec(function(err,data){
-				console.log(data.puntuacion);
-				res.json(data);
-			});
+		Profesor.findOne({userName: req.params.id}).exec(function(err,data){
+			console.log(data.valoracionMedia);
+			res.json(data);
 		});
 	});
 };
