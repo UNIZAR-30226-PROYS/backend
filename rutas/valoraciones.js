9d8ab8ef27fb0e22b0611ebb689d889c823e2001
jjgr3496 <jjgomez96@hotmail.com>
2017-04-22 15:20:07 +0200

    Addicion de valoraciones y pseudo-trigger
    Inclusion de valoraciones y actualizacion automatica de la puntuacion media de cada profesor.

--- rutas/valoraciones.js
+++ rutas/valoraciones.js
@@ -12,4 +12,28 @@ module.exports = function (app)
 			res.json(data);
 		});
 	});
+
+	app.post('api/valoraciones/valorar', function(req, res){
+
+		if (req.decoded && !_.isEmpty(req))
+		{
+			
+			Valoracion.insertOne(profesor:req.decoded.profesorID,alumno:req.decoded.alumnoID, puntuacion:req.decoded.puntuacion);
+			Profesor.findOne({_id:req.decoded.profesorID}, function(err,data_user){
+								var mu = data_user.valoracionMedia;
+								var num_val = data_user.numeroValoraciones;
+								mu = mu * num_val + req.decoded.puntuacion;
+								data_user.numeroValoraciones = num_val + 1;
+								data_user.valoracionMedia = mu/num_val;
+								data_user.save();
+								});
+		} else
+		{
+			res.status(500).json({
+				success: false,
+				message: 'Se ha prohibido el acceso'
+			});
+		}
+
+	});
 };
