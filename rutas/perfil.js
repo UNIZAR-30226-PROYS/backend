3b76a5b81be01295de2d10e04414fc86aebb11f8
Xewir <miguelcrespo.chiqui@gmail.com>
2017-05-10 14:41:52 +0200

    Soporte de modificaciones completado

--- rutas/perfil.js
+++ rutas/perfil.js
@@ -5,12 +5,15 @@ var modelos = Array(n_modelos);
 modelos[0] = require('../modelos/alumno');
 modelos[1] = require('../modelos/profesor');
 
+var Asignatura = require('../modelos/asignatura');
+
 var auth = require('../auth');
 var _ = require('lodash');
+var async = require('async');
 
 module.exports = function (app) 
 {
-	app.get('/api/perfil/info', function(req,res) {
+	app.get('/api/perfil/info', auth, function(req,res) {
 		// Solo se devolverá la info si el usuario se encuentra logeado
 		if (req.decoded) 
 		{
@@ -35,26 +38,10 @@ module.exports = function (app)
 		}
 		else
 		{
-			/*res.status(500).json({
+			res.status(500).json({
 				success: false,
 				message: 'Se ha prohibido el acceso'
-			});*/
-			modelos[1].findOne({userName: "profesor1"}, { _id: 0, __v: 0, password: 0, session: 0})
-				                         .populate("asignaturas", ["nombre"])
-				                         .select("-_id")
-				                         .exec(function(error, data) {
-					if (error || !data)
-					{
-						res.status(403).json({
-							success: false,
-							message: 'Error obteniendo la informacion'
-						});
-					} 
-					else 
-					{
-						res.json(data);
-					}
-				});
+			});
 		}
 	});
 
@@ -66,7 +53,9 @@ module.exports = function (app)
 			// Es un alumno
 			if(req.decoded.tipo >= 0 && req.decoded.tipo < n_modelos)
 			{
-				modelos[req.decoded.tipo].findOne({userName: req.decoded.user.userName}, function(error, data) {
+				modelos[req.decoded.tipo].findOne({userName: req.decoded.user.userName}, {_id: 0, __v: 0, password: 0, session: 0})
+									     .populate("asignaturas", ["nombre"])
+									     .exec(function(error, data) {
 					if (error || !data)
 					{
 						res.status(403).json({
@@ -103,20 +92,73 @@ module.exports = function (app)
 		{
 			if(req.decoded.tipo >= 0 && req.decoded.tipo < n_modelos)
 			{
-				console.log(req.body);
-				modelos[req.decoded.tipo].update({userName: req.decoded.user.userName}, req.body, function(err, n_updates) {
-					if (err || n_updates == 0)
+				modelos[req.decoded.tipo].findOne({userName: req.decoded.userName}, function(err, data){
+					if (err || !data)
 					{
 						res.status(500).json({
 							success: false,
 							message: 'Error modificando la informacion'
 						});
-					}
-					else
-					{
-						res.status(200).json({
-							success: true
-						})
+					} else {
+
+						for (var property in req.body)
+						{
+							if (data[property] != undefined && property !== "asignaturas")
+							{
+								data[property] = req.body[property];
+							}
+						}
+
+						if (req.body.asignaturas != undefined)
+						{
+							var asignaturas = req.body.asignaturas.split(",");
+							var n = asignaturas.length;
+							var i = 0;
+
+							async.each(asignaturas, function(asig, next){
+								Asignatura.findOne({nombre: asig}, function(err,data){
+									if (!err)
+									{
+										data.asignaturas.push(data._id);
+										i++;
+
+										if (i == n)
+										{	
+											data.save(function(err, me) {
+												if (err) {
+													res.status(500).json({
+														success: false,
+														message: 'Error modificando la informacion'
+													});
+												} else {
+													console.log(chalk.green("Modificado usuario " + me.userName));
+													res.status(200).json({
+														success: true,
+														message: 'Modificado correctamente el usuario'
+													});
+												}
+											});
+										}
+									}
+									next();
+								});
+							});
+						} else {
+							data.save(function(err, me) {
+								if (err) {
+									res.status(500).json({
+										success: false,
+										message: 'Error modificando la informacion'
+									});
+								} else {
+									console.log(chalk.green("Modificado usuario " + me.userName));
+									res.status(200).json({
+										success: true,
+										message: 'Modificado correctamente el usuario'
+									});
+								}
+							});
+						}
 					}
 				});
 			}
