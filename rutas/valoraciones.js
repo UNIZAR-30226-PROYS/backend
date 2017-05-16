3dd507da7329f0ce662ef8e7c6f9125db95f5453
enrique <epalacins@gmail.com>
2017-05-17 00:20:27 +0200

    valoracion (pendiente de pruebas)

--- rutas/valoraciones.js
+++ rutas/valoraciones.js
@@ -1,39 +1,85 @@
 
-require('../modelos/alumno');
-require('../modelos/profesor');
-require('../modelos/valoracion');
+var Alumno = require('../modelos/alumno');
+var Profesor = require('../modelos/profesor');
+var auth = require('../auth');
+var _ = require('lodash');
+var Valoracion = require('../modelos/valoracion');
 
-
-module.exports = function (app) 
-{
+module.exports = function (app) {
 	app.get('/api/valoraciones/:id', function (req, res) {
-		Profesor.findOne({userName: req.params.id}).exec(function(err,data){
-			console.log(data.valoracionMedia);
-			res.json(data);
-		});
-	});
-
-	app.post('api/valoraciones/valorar', function(req, res){
-
-		if (req.decoded && !_.isEmpty(req))
-		{
-			
-			Valoracion.insertOne({profesor:req.decoded.profesorID,alumno:req.decoded.alumnoID, puntuacion:req.decoded.puntuacion});
-			Profesor.findOne({_id:req.decoded.profesorID}, function(err,data_user){
-								var mu = data_user.valoracionMedia;
-								var num_val = data_user.numeroValoraciones;
-								mu = mu * num_val + req.decoded.puntuacion;
-								data_user.numeroValoraciones = num_val + 1;
-								data_user.valoracionMedia = mu/num_val;
-								data_user.save();
-								});
-		} else
-		{
+		if (req.decoded && !_.isEmpty(req)) {
+			Profesor.findOne({userName: req.params.id}).exec(function (err, data) {
+				console.log(data.valoracionMedia);
+				res.json(data);
+			});
+		} else {
 			res.status(500).json({
 				success: false,
 				message: 'Se ha prohibido el acceso'
 			});
 		}
-
 	});
-};
+
+	app.post('/api/valoraciones/valorar', auth, function (req, res) {
+
+		if (req.decoded && !_.isEmpty(req) && req.decoded.tipo == 0) {
+			// console.log(req)
+			Valoracion.findOne({profesor: req.body.profesorID, alumno: req.decoded.user._id}, function (err, data) {
+				if (err) {
+					console.log("Error en la busqueda de la valoracion");
+					console.log(err);
+					res.status(403).json({
+						success: false,
+						message: 'Error en la busqueda'
+					});
+				}
+				else if (data) {
+					console.log("Valoracion repetida");
+					console.log(data);
+					res.status(403).json({
+						success: false,
+						message: 'El usuario ya ha valorado anteriormente a este profesor'
+					});
+				}
+				else {
+					datos = {
+						profesor: req.body.profesorID,
+						alumno: req.decoded.user._id,
+						puntuacion: req.body.puntuacion
+					};
+					var val = new Valoracion(datos);
+					val.save();
+					actualizarValMedia(req, res)
+				}
+			});
+		} else {
+			console.log("error")
+			res.status(500).json({
+				success: false,
+				message: 'Se ha prohibido el acceso'
+			});
+		}
+	});
+}
+
+function actualizarValMedia(req, res){
+	Profesor.findOne({_id:req.body.profesorID}, function(err,data_user) {
+		if (err || !data_user) {
+			console.log("Error en la busqueda");
+			console.log(err);
+			res.status(403).json({
+				success: false,
+				message: 'Error en la busqueda'
+			});
+		}
+		else {
+			//console.log(data_user)
+			var mu = data_user.valoracionMedia;
+			var num_val = data_user.numeroValoraciones;
+			mu = mu * num_val + req.body.puntuacion;
+			data_user.numeroValoraciones = num_val + 1;
+			data_user.valoracionMedia = mu / (num_val + 1);
+			data_user.save();
+		}
+	});
+}
